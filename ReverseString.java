a5c196ec3a9a5ee49886bb0f5e3ace7e125361d2
Brundha M <brundhamohan11@gmail.com>
2026-03-18 22:14:48 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ReverseString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        char[] arr=str.toCharArray();
+        int left=0;
+        int right=arr.length-1;
+        while(left<right){
+            char temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+        String rev=new String(arr);
+        System.out.println(rev);
+    }
+}
